78f4d21d46078aa8b904c46ab56c272d60715826
PeterJamesO <pete141@live.co.uk>
2017-04-13 13:30:32 +0100

    Added Call Setup Screen

---
+++ app/src/main/java/whsct/test/userCallScreen.java
@@ -0,0 +1,13 @@
+package whsct.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class userCallScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_call_screen);
+    }
+}
