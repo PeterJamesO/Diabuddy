cbc51773c212c2df46f079461a68a51ca627c97c
Jamie Gardner <jamiedanielgardner@outlook.com>
2017-04-17 13:37:30 +0100

    Removed UserCallScreen and -Setup Added call functionality foundations

--- app/src/main/java/whsct/test/MainMenu.java
+++ app/src/main/java/whsct/test/MainMenu.java
@@ -1,13 +1,23 @@
 package whsct.test;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainMenu extends AppCompatActivity {
     public static final String NAME = "com.whsct.MESSAGE";
+    private final Context context = this;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +29,27 @@ public class MainMenu extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.NAME);
         TextView name = (TextView) findViewById(R.id.welcomeText);
         name.setText(message);
+
+        button = (Button) findViewById(R.id.callButton);
+
+        // Add phone state listener
+        PhoneCallListener callListener = new PhoneCallListener();
+        TelephonyManager telephonyManager = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        telephonyManager.listen(callListener, PhoneStateListener.LISTEN_CALL_STATE);
+
+        // Add button listener
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:07422661220"));
+                startActivity(callIntent);
+            }
+        });
     }
 
     public void callButton(View view) {
-        // Go to emergency screen
-        Intent intent = new Intent(this, userCallSetupScreen.class);
-        startActivity(intent);
+
     }
 
     public void notesButton(View view) {
@@ -49,4 +74,9 @@ public class MainMenu extends AppCompatActivity {
     public void onBackPressed(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);    }
+
+    private class PhoneCallListener extends PhoneStateListener {
+        private boolean isPhoneCalling = false;
+
+    }
 }
